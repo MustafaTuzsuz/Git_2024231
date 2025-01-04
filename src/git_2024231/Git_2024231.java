12cbcb11d02c77ddacb0769c44ec4f1ce3626986
MustafaTuzsuz <161529001+MustafaTuzsuz@users.noreply.github.com>
2025-01-04 05:15:46 +0000

    added personal info

---
+++ src/git_2024231/Git_2024231.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2024231;
+
+/**
+ *
+ * @author talha
+ */
+public class Git_2024231 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Mustafa Talha Tuzsuz   -    2024231");
+        
+    }
+    
+}
